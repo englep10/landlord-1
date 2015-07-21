78441c29141c0bd16cb6e84025102a6d47b77f88
Philip Engles <philipengles@Philips-MacBook-Pro-2.local>
2015-07-21 10:05:46 -0400

    finished HW;

---
+++ models/tenant.rb
@@ -0,0 +1,12 @@
+class Tenant
+  attr_accessor :name, :age, :gender
+
+  def initialize(name, age, gender)
+    @name = name
+    @age = age
+    @gender = gender
+  end
+end
+
+
+puts "exercise complete" # fixes a bug in pry
